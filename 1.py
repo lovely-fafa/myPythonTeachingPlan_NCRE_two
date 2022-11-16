0ee877d0afec4e7120e11c5620abcafaab592cf0
fafa <13619088487@163.com>
2022-11-16 23:31:11 +0800

    day01

---
+++ 1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python 3.10
+# -*- coding: utf-8 -*- 
+#
+# @Time    : 2022-11-16 23:16
+# @Author  : 发发
+# @QQ      : 1315337973
+# @File    : 1.py
+# @Software: PyCharm
+print(range(10))
+print(type(range(10)))  # 可迭代对象
+for i in [0, 1, 2, 3, 0, 1, 2, 3,0, 1, 2]:
+    print(i)
--- day01/0.py
+++ day01/0.py
@@ -1,23 +1,10 @@
 # 字符串的一个方法 str.format() 代替 %
+# format 格式化输出的函数
 print("我叫{}，今年{}岁。".format("发发", 2))
 print("我叫{}，今年{}岁。".format(3, "发发"))
 
-# 可以看到，format函数接收的是一个元祖，故可：
-
-num_tuple = ("发发", 3)
-print("我叫{}，今年{}岁。".format(*num_tuple))
-# 为什么这样不可以？
-
-print("我叫{0}，{0}今年{1}岁。".format("发发", 3))
-
 # 亦可使用关键词
 print("我叫{name}，{name}今年{grade}年级。".format(name="发发", grade=5))
-
-# 亦可用列表 (注意应该是“{0[0]}”，而不是“{list1[0]}”)
-list1 = ["发发", 3]
-list2 = ["xx", 2]
-print("我是{0[0]}，今年{0[1]}！\n她是{1[0]}，今年{1[1]}岁。".format(list1, list2))
-
 
 # 格式化数字(记得有:)
 print("{:.5f}".format(3.14))  # 保留小数点
